ebabe31b355be37508588bb09e002792a0fe6be7
sapieninja <60101890+sapieninja@users.noreply.github.com>
2020-06-24 19:31:00 +0100

    reorganised file system

--- errorhandling.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-class errorhandling(commands.Cog):
-    "basic error handling"
-    @commands.Cog.listener()#will check for an event happening and call the coroutine on that event.
-    async def on_command_error(self,ctx,error):
-        await ctx.send('{0.mention} that command has resulted in the following error: {1}'.format(ctx.author,error))
-def setup(bot):
-    bot.add_cog(errorhandling(bot))
