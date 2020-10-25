12b1f3994c5290a62e903010dadadda997bdf372
Max Bowman <maxjbowman@gmail.com>
2020-10-25 15:40:08 +0000

    Final Commit (i hope)

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
 import discord
 from discord.ext import commands
 import json
-bot = commands.Bot(command_prefix='!',description="Bot to help with some functionality on speedcubers server")
-with open('info.json','r') as info_file:
-    info = json.load(info_file) 
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!',
+                   description="Bot to help with some functionality on speedcubers server",
+                   intents=intents)
+with open('info.json', 'r') as info_file:
+    info = json.load(info_file)
 token = info['token']
-with open("loadedcogs.json",'r') as cogs_file:
+with open("loadedcogs.json", 'r') as cogs_file:
     cogs = json.load(cogs_file)
 if __name__ == '__main__':
     for cog in cogs:
         bot.load_extension(cog)
+
+
 @bot.event
 async def on_ready():
     print(f'logged in as {bot.user.name}')
+
+
 @bot.command()
 @commands.has_role('4x4 (mods)')
-async def addcog(ctx,cogname):
-    with open("loadedcogs.json","r") as cogs_file:
+async def addcog(ctx, cogname):
+    with open("loadedcogs.json", "r") as cogs_file:
         cogs = json.load(cogs_file)
     cogname = 'cogs.' + cogname
     if cogname not in cogs:
@@ -25,12 +33,15 @@ async def addcog(ctx,cogname):
             cogs.append(cogname)
         except:
             raise ValueError('You moron that cog does not exist')
-    with open("loadedcogs.json",'w') as cogs_file:
-        json.dump(cogs,cogs_file)
+    with open("loadedcogs.json", 'w') as cogs_file:
+        json.dump(cogs, cogs_file)
+    print(f"added {cogname}")
+
+
 @bot.command()
 @commands.has_role('4x4 (mods)')
-async def removecog(ctx,cogname):
-    with open("loadedcogs.json","r") as cogs_file:
+async def removecog(ctx, cogname):
+    with open("loadedcogs.json", "r") as cogs_file:
         cogs = json.load(cogs_file)
     cogname = 'cogs.' + cogname
     try:
@@ -38,13 +49,16 @@ async def removecog(ctx,cogname):
         cogs.remove(cogname)
     except:
         raise ValueError('You did something wrong idk')
-    with open("loadedcogs.json","w") as cogs_file:
-        json.dump(cogs,cogs_file)         
+    with open("loadedcogs.json", "w") as cogs_file:
+        json.dump(cogs, cogs_file)
+    await ctx.send(f"removed {cogname}")
+
+
 @bot.command()
 @commands.has_role('4x4 (mods)')
-async def refreshcog(ctx,cogname):
-    removecog(ctx,cogname)
-    addcog(ctx,cogname)
-    
+async def refreshcog(ctx, cogname):
+    await removecog(ctx, cogname)
+    await addcog(ctx, cogname)
+    await ctx.send(f"refreshed {cogname}")
+
 bot.run(token)
-        
--- cogs/reactionroles.py
+++ cogs/reactionroles.py
@@ -8,26 +8,33 @@ else:
     raise RuntimeError('running as __main__. Run bot.py instead')
 
 
-class reactionroles(commands.Cog):#defines a new class inheriting from commands.Cog
-    "simple command frontend for my equation class"
-    def roles_info(self,payload):
-        with open('info.json','r') as info_file:
+class reactionroles(commands.Cog):  # defines a new class inheriting from commands.Cog
+    "simple role on reaction code"
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        with open('info.json', 'r') as info_file:
             info = json.load(info_file)
-        if payload.message_id != info['reaction_message_id']:
-            return None
-        guild = bot.get_guild(payload.guild_id)
-        role = guild.get_role(info["role_id"])#
-        return role,guild
+        message_id = payload.message_id
+        guild_id = payload.guild_id
+        if message_id == info['reaction_message_id']:
+            guild = bot.get_guild(guild_id)
+            member = guild.get_member(payload.user_id)
+            role = guild.get_role(info['role_id'])
+            await member.add_roles(role)
+
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self,payload):
-        role, guild= self.roles_info(payload)
-        if role is not None: #avoid some console spam (not necessary but nice to have)
-            await payload.member.add_roles(role)
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self,payload):
-        role, guild = self.roles_info(payload)
-        if role is not None:
-            member = guild.get_member(payload.user_id) #payload does not have a member subclass in this occasion-
+    async def on_raw_reaction_remove(self, payload):
+        with open('info.json', 'r') as info_file:
+            info = json.load(info_file)
+        message_id = payload.message_id
+        guild_id = payload.guild_id
+        if message_id == info['reaction_message_id']:
+            guild = bot.get_guild(guild_id)
+            member = guild.get_member(payload.user_id)
+            role = guild.get_role(info['role_id'])
             await member.remove_roles(role)
+
+
 def setup(bot):
     bot.add_cog(reactionroles(bot))
+    print("loaded reactionroles")
